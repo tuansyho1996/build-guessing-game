1d231f34cd251561e6f3b24929e6b3a1d7ae5e27
tuansyho1996 <hosytuan101296@gmail.com>
2024-09-26 23:50:32 +0700

    feat: add no align query psql file number_guess

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
+PSQL="psql -X --username=freecodecamp --dbname=number_guess --no-align --tuples-only -c"
 
 ENTER_NUMBER_GUESS() {
   echo "That is not an integer, guess again:"
